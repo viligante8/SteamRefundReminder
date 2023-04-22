0e8deb0b762aa41396489d6ac9fe157a867a997b
Vito Pistelli <vito.pistelli@razartech.com>
2023-04-22 15:16:50 -0400

    remove key

--- src/lib/steam_service.ts
+++ src/lib/steam_service.ts
@@ -3,7 +3,6 @@ import { GetOwnedGamesResponse, OwnedGames } from '../types';
 
 const getOwnedGames = async (): Promise<OwnedGames> => {
   const url = 'http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/';
-  // '?key=E2DA8C65D7A79D392F89C609BA0CF86D&steamid=76561198025429116&format=json',
 
   const result = await axios.get<GetOwnedGamesResponse>(url, {
     params: {
